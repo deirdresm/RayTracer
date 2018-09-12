338cdcd27f102497d14822b50b2c715e89da5711
Deirdre Saoirse Moen <deirdre@deirdre.net>
2018-09-12 16:52:08 -0700

    Initial Commit

---
+++ RayTracer/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  RayTracer
+//
+//  Created by Deirdre Saoirse Moen on 9/12/18.
+//  Copyright © 2018 Deirdre Saoirse Moen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
