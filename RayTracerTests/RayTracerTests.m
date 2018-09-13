d8c747d3f0271bf6754fe7b2c6d0a73a6233cca3
Deirdre Saoirse Moen <deirdre@deirdre.net>
2018-09-12 17:04:09 -0700

    Adding ignore and attributes files.

---
+++ RayTracerTests/RayTracerTests.m
@@ -0,0 +1,9 @@
+//
+//  RayTracerTests.m
+//  RayTracerTests
+//
+//  Created by Deirdre Saoirse Moen on 9/12/18.
+//  Copyright © 2018 Deirdre Saoirse Moen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
